2f2b1ffe6c848ae64f6400652d1fb4d9bcd824b0
Eloy Manuel Chacón Martínez <contactos@miguayaba.com>
2014-10-17 07:28:28 -0500

    Actualizacion Importante

--- modules/inc/menu1.inc.php
+++ modules/inc/menu1.inc.php
@@ -1,35 +1,17 @@
 					<ul class="menu1">
-				  <li><a href="index.php" ><img src="images/site/home.png" alt="" style="vertical-align:middle;" ></a></li>
 						<?php do { ?>	
 							<?php if($row_menu1['tipo_link']==1) { ?>
 							<li>
 								<a href="index.php?mod=<?php echo $row_menu1['alias'];?>" ><?php echo $row_menu1['titulo_link']; ?></a>
 								<!-- SUB MENUS -->
-								<?php if($row_menu1['alias']=="nosotros")  {?>
-								<ul>
-									<li><a href="index.php?mod=servicio">Servicio</a></li>
-								   <li><a href="index.php?mod=filosofia">Filosofia</a></li>
-								   <li><a href="index.php?mod=directivos">Directivos</a></li>
-								</ul>
-								<?php } ?>
+								
 								<?php if($row_menu1['alias']=="clientes")  {?>
 								<ul>
 									<li><a href="index.php?mod=clientes">Clientes</a></li>
 								   <li><a href="index.php?mod=testimoniales">Testimoniales</a></li>
 								</ul>
 								<?php } ?>
-								<?php if($row_menu1['alias']=="servicios")  {?>
-								<ul>
-									<li><a href="index.php?mod=marketing-360">Marketing 360</a></li>
-								   <li><a href="index.php?mod=comunicacion-corporativa">Comunicaci&oacute;n Corporativa</a></li>
-								   <li><a href="index.php?mod=redes-sociales">Redes Sociales</a></li>
-								   <li><a href="index.php?mod=imagen-corporativa">Imagen Corporativa</a></li>
-								   <li><a href="index.php?mod=eventos-y-btl">Eventos y BTL</a></li>
-								   <li><a href="index.php?mod=creatividad-y-publicidad">Creatividad y publicidad</a></li>
-								   <li><a href="index.php?mod=trade-marketing">Trade Marketing</a></li>
-								   <li><a href="index.php?mod=inteligencia-de-mercado">Inteligencia de Mercado</a></li>
-								</ul>
-								<?php } ?>
+								
 							</li>
 							<?php } ?>
 							<?php if($row_menu1['tipo_link']==2) { ?>
